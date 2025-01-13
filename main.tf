146fd9962f8351bb59f5fc425303af03aaf3262c
haiyue.yuan <haiyue.yuan@gmail.com>
2025-01-13 16:29:55 +0900

    add ec2 deployment config

--- main.tf
+++ main.tf
@@ -1,19 +1,117 @@
 provider "aws" {}
 
-variable "cidr_block" {
-  description = "cidr block"
-  type = list(string)
-}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
 
-resource "aws_vpc" "k8s-vpc" {
-  cidr_block = var.cidr_block[0]
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name: "kubernetes-cluster"
+    Name: "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "k8s-subnet-1" {
-  vpc_id = aws_vpc.k8s-vpc.id
-  cidr_block = var.cidr_block[1]
-  availability_zone = "ap-northeast-1a"
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name: "${var.env_prefix}-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name: "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name: "${var.env_prefix}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-sg"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["al2023-ami-2023.6.20250107.0-kernel-*-x86_64"]
+  }
+}
+
+# output "aws_ami_id" {
+#   value = data.aws_ami.latest-amazon-linux.id
+# }
+
+resource "aws_instance" "myapp-ec2" {
+  ami = data.aws_ami.latest-amazon-linux.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = "server-key-pair"
+
+  user_data = file("entry-script.sh")
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name: "${var.env_prefix}-server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.myapp-ec2.public_ip
 }
