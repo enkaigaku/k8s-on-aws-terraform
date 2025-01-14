a0c3fd5eed7a5944e2fb6eff3806e217a28c08a1
haiyue.yuan <haiyue.yuan@gmail.com>
2025-01-14 22:46:16 +0900

    modularize project

---
+++ modules/webserver/main.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-sg"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = [var.image_name]
+  }
+}
+
+resource "aws_instance" "myapp-ec2" {
+  ami = data.aws_ami.latest-amazon-linux.id
+  instance_type = var.instance_type
+
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = "server-key-pair"
+
+  user_data = file("./modules/webserver/entry-script.sh")
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name: "${var.env_prefix}-server"
+  }
+}
---
+++ modules/webserver/variables.tf
@@ -0,0 +1,7 @@
+variable my_ip {}
+variable env_prefix {}
+variable instance_type {}
+variable avail_zone {}
+variable subnet_id {}
+variable image_name {}
+variable vpc_id {}
