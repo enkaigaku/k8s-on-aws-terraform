9a2e755238210f5971ee1a5dedad3cf52397a942
haiyue.yuan <haiyue.yuan@gmail.com>
2025-01-15 22:23:11 +0900

    launch multiple instance

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "ec2_public_ip" {
-  value = module.myapp-webserver.instance.public_ip
-}
+# output "webserver_1_public_ip" {
+#   value = module.myapp-webserver-1.instance.public_ip
+# }
+
+# output "webserver_2_public_ip" {
+#   value = module.myapp-webserver-2.instance.public_ip
+# }
